512aa99441dc9d51efa32ab41d92371d122a2123
Mizuhashi <asdfg33484221@gmail.com>
2025-01-25 16:06:07 -0800

    Lab3 step 2 finished

--- global.js
+++ global.js
@@ -3,3 +3,17 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+// label current link as class 'current'
+let navLinks = $$("nav a");
+let currentLink = navLinks.find(
+    (a) => a.host === location.host && a.pathname === location.pathname
+);
+
+if (currentLink) {
+    // or if (currentLink !== undefined)
+    currentLink.classList.add('current');
+}
+// or using the following just for prevent error
+// currentLink?.classList.add('current');
+
